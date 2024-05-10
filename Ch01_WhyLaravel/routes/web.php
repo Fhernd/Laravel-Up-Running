d429bc54bfd9555fde4f38986a113a55db9dfcc3
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-10 05:25:15 -0500

    Create a Greeting instance from a route

--- Ch01_WhyLaravel/routes/web.php
+++ Ch01_WhyLaravel/routes/web.php
@@ -4,8 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\WelcomeController;
 
+use App\Models\Greeting;
+
 Route::get('/', function () {
     return 'Hello, World!';
 });
 
 Route::get('/welcome', [WelcomeController::class, 'index']);
+
+
+Route::get('/greeting-create', function () {
+    $greeting = new Greeting();
+    $greeting->body = 'Hello, World!';
+    $greeting->save();
+    return 'Greeting created';
+});

85783dd39da8dfe41adf5390bc0c20b47f199a62
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-10 05:03:43 -0500

    WelcomeController usage from web.php

--- Ch01_WhyLaravel/routes/web.php
+++ Ch01_WhyLaravel/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WelcomeController;
+
 Route::get('/', function () {
     return 'Hello, World!';
 });
+
+Route::get('/welcome', [WelcomeController::class, 'index']);

cedd9d0c014f6f5c9d99be2e01580fe1f784783d
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-22 19:22:05 -0500

    Tratando de resolver el problema de creación de una tarea

--- Ch03_Routing_Controllers/routes/web.php
+++ Ch03_Routing_Controllers/routes/web.php
@@ -68,4 +68,5 @@ Route::get('home', function () {
 })->name('home');
 
 
-Route::get('/tasks',[TaskController::class, 'index']);
+Route::get('/tasks/create',[ TaskController::class, 'create']);
+Route::post('/tasks',[ TaskController::class, 'store']);

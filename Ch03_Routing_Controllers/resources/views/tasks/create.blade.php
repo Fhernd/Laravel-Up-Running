6b16f68dc403530fdfbc35bcd4203482721f6eeb
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-22 19:41:13 -0500

    Formulario para crear una tarea

--- Ch03_Routing_Controllers/resources/views/tasks/create.blade.php
+++ Ch03_Routing_Controllers/resources/views/tasks/create.blade.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html>
-<body>
+@extends('layout')
 
-<h2>Create Task</h2>
-
-<form action="/task" method="POST">
-    @csrf
-    <label for="title">Title:</label><br>
-    <input type="text" id="title" name="title" required><br>
-    <label for="description">Description:</label><br>
-    <textarea id="description" name="description" required></textarea><br>
-    <input type="submit" value="Submit">
-</form> 
-
-</body>
-</html>
+@section('content')
+    <h1>Create Task</h1>
+    <form action="{{ route('tasks.store') }}" method="POST">
+        @csrf
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title">
+        <br>
+        <label for="description">Description:</label>
+        <textarea name="description" id="description"></textarea>
+        <br>
+        <button type="submit">Create</button>
+    </form>
+@endsection

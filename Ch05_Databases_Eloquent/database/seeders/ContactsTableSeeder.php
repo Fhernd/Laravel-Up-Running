a954f9d67251a6dee5afa9ffc57cd4922430b176
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-08-01 06:25:59 -0500

    Seeder data for the contacts table

--- Ch05_Databases_Eloquent/database/seeders/ContactsTableSeeder.php
+++ Ch05_Databases_Eloquent/database/seeders/ContactsTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class ContactsTableSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class ContactsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        
+        // User IDs
+        $userIds = [2, 4];
+
+        foreach ($userIds as $userId) {
+            // Create up to 5 contacts for each user
+            $numberOfContacts = rand(1, 5);
+            for ($i = 0; $i < $numberOfContacts; $i++) {
+                DB::table('contacts')->insert([
+                    'name' => $faker->name,
+                    'email' => $faker->unique()->safeEmail,
+                    'phone' => $faker->phoneNumber,
+                    'user_id' => $userId,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
     }
 }
